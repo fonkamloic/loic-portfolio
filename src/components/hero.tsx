36534882931bcc46229d1829c9958bc6a19efa26
Christian Asnel Ngoulla Sob <ngoullasob@gmail.com>
2024-05-12 23:17:22 +0200

    Add basic hero animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ArrowRightIcon } from "lucide-react";
 import React from "react";
 import { Button, buttonVariants } from "./ui/button";
@@ -5,14 +7,41 @@ import { personalInformation } from "@/app/data";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { time } from "console";
 
 function Hero() {
+  useGSAP(() => {
+    gsap.to(".about-div", {
+      opacity: 1,
+      x: 0,
+      duration: 1.5,
+      delay: 0.2,
+      ease: "power4.out",
+    });
+    gsap.to(".img-container", {
+      opacity: 1,
+      x: 0,
+      rotate: "-6deg",
+      duration: 1.5,
+      delay: 0.2,
+      ease: "power4.out",
+    });
+    gsap.to(".img-container-div", {
+      rotate: "12deg",
+      duration: 1.2,
+      delay: 0.2,
+      ease: "power4.out",
+    });
+  }, []);
+
   return (
     <section
       id="about"
       className="max-w-6xl mx-auto min-w-0 flex flex-col flex-wrap gap-8 md:flex-row px-8 md:min-h-[calc(100vh-6rem)] md:items-center overflow-hidden py-12 pb-32 md:py-0"
     >
-      <div className="flex-1 flex flex-col justify-between max-h-[35rem] min-w-0">
+      <div className="flex-1 flex flex-col justify-between max-h-[35rem] min-w-0 opacity-0 about-div -translate-x-200">
         <div className="flex flex-col gap-4 flex-1">
           <h3 className="text-primary text-xl font-light tracking-wider">
             Hi, I&apos;m
@@ -48,8 +77,8 @@ function Hero() {
         </div>
       </div>
       <div className="flex-1 self-stretch flex items-center justify-end min-w-0 mr-8">
-        <div className="container relative shadow-md z-10 w-full -rotate-6 h-full origin-center min-w-[15rem] min-h-[18rem] max-h-[28rem] max-w-[22rem] md:max-h-[25rem] md:max-w-[25rem] bg-background/[0.1] border border-background/[0.5] rounded-xl">
-          <div className="back-container absolute w-full h-full bg-background/[0.2] shadow-md rounded-xl rotate-12 backdrop-blur origin-center border border-background/[0.5]">
+        <div className="container img-container relative shadow-md z-10 w-full opacity-0 translate-x-100 -rotate-0 h-full origin-center min-w-[15rem] min-h-[18rem] max-h-[28rem] max-w-[22rem] md:max-h-[25rem] md:max-w-[25rem] bg-background/[0.1] border border-background/[0.5] rounded-xl">
+          <div className="back-container img-container-div absolute w-full h-full bg-background/[0.2] shadow-md rounded-xl rotate-0 backdrop-blur origin-center border border-background/[0.5]">
             <Image
               src={personalInformation.picture.src}
               alt={personalInformation.picture.alt}
